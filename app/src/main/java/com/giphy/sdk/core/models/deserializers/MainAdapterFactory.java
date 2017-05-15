5f19cfb82629ccc3360af9ed2d9222835a5154b1
bogdantmm92 <bogdantirca92@gmail.com>
2017-05-15 19:38:41 +0300

    Add license (#28)
    * Add licence
    
    * Add license to test files
    
    * Update copyright
    
    * Fix copyright format

--- app/src/main/java/com/giphy/sdk/core/models/deserializers/MainAdapterFactory.java
+++ app/src/main/java/com/giphy/sdk/core/models/deserializers/MainAdapterFactory.java
@@ -1,6 +1,28 @@
+/*
+ * Created by Bogdan Tirca on 5/9/17.
+ * Copyright (c) 2017 Giphy Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.giphy.sdk.core.models.deserializers;
 
-import com.giphy.sdk.core.models.Images;
 import com.giphy.sdk.core.models.Media;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
@@ -10,10 +32,6 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
-
-/**
- * Created by bogdantmm on 5/9/17.
- */
 
 public class MainAdapterFactory implements TypeAdapterFactory {
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
--- app/src/main/java/com/giphy/sdk/core/network/engine/DefaultNetworkSession.java
+++ app/src/main/java/com/giphy/sdk/core/network/engine/DefaultNetworkSession.java
@@ -1,3 +1,26 @@
+/*
+ * Created by Bogdan Tirca on 4/19/17.
+ * Copyright (c) 2017 Giphy Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.giphy.sdk.core.network.engine;
 
 import android.net.Uri;
@@ -25,10 +48,6 @@ import java.net.URL;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.Callable;
-
-/**
- * Created by bogdantmm on 4/19/17.
- */
 
 public class DefaultNetworkSession implements NetworkSession {
     private static final Gson GSON_INSTANCE = new GsonBuilder()
